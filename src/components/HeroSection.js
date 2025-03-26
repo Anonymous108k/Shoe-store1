fc9b37b3dd5fde881dd133daaa839db948489944
Pooja <survepooja108@gmail.com>
2025-03-26 15:30:10 +0530

    imp

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,11 +14,11 @@ const HeroSection = () => {
     }}
     >
       <div className="container text-start">
-        <h1 className="display-3 fw-light mb-3">
+        <h1 className="display-3 mb-3">
           <span>STEP INTO STYLE</span>
           WITH STRIDE!
         </h1>
-        <p className="fs-5 mb-3">
+        <p className="fs-4 ">
           Discover the perfect pair for every occasion and step out <br /> in style, comfort, and confidence.
           Let your stride speak for itself!
         </p>
